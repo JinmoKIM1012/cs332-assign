591406b90f0003f0cc910d8d7ae346866d433c5f
Jinmo Kim <jmkim1012@postech.ac.kr>
2022-09-14 13:43:35 +0900

    Solve assignment #1 - recfun
    Solved exercise #1 with a simple recurrence relation of Pascal's
    triangle.
    Solved exercise #2 by counting 'open' brace and 'close' brace with
    simple tail recursion.
    Solved exercise #3 by simple recurrence relation.

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,6 +1,8 @@
 package recfun
 import common._
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -14,15 +16,33 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def balanceLoop(chars: List[Char], cnt: Int): Boolean = {
+      if (cnt < 0) false
+      else if (chars.isEmpty) cnt == 0
+      else if (chars.head == '(') balanceLoop(chars.tail, cnt + 1)
+      else if (chars.head == ')') balanceLoop(chars.tail, cnt - 1)
+      else balanceLoop(chars.tail, cnt)
+    }
+
+    balanceLoop(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    // impossible cases
+    if (money < 0 || coins.isEmpty) 0
+    // if money is 0, there is only one way - '0'
+    else if (money == 0) 1
+    // use or not use first coin type
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
